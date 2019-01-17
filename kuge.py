d36881aef8daf40ea967d7355470a467af6182e6
Futafi <D.futagi@vivaldi.net>
2019-01-18 00:19:21 +0900

    ざっといい感じに作った

---
+++ kuge.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+
+
+def get(is_headless=True, is_photo=True, path_to_chrome_driver=""):
+    options = webdriver.ChromeOptions()
+    if is_headless:
+        options.add_argument('--headless')
+    if not is_photo:
+        options.add_argument('--blink-settings=imagesEnabled=false')
+
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--lang=ja')
+    options.add_argument('--disable-desktop-notifications')
+
+    # 現時点で拡張機能の使用は不可能だがGoogleがそのうち使えるようにしそうなのであらかじめdisableしておく
+    options.add_argument("--disable-extensions")
+
+    if path_to_chrome_driver:
+        browser = webdriver.Chrome(executable_path=path_to_chrome_driver, options=options)
+        browser.implicitly_wait(3)
+        return browser
+
+    try:
+        import chromedriver_binary
+    except ModuleNotFoundError:
+        browser = webdriver.Chrome(executable_path='/usr/lib/chromium-browser/chromedriver', options=options)
+    else:
+        browser = webdriver.Chrome(options=options)
+
+    browser.implicitly_wait(3)
+    return browser
